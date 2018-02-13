035501a23c58d0e1b397e5ab27cfbf7a18dca4dd
pengtao <pengtao@cnhnkj.com>
2018-02-13 17:16:13 +0800

    适配

--- js/radio.js
+++ js/radio.js
@@ -7,7 +7,7 @@
             if (n) {
                 var whdef = 100/1920;// 表示1920的设计图,使用100PX的默认值
                 var wW = window.innerWidth * (window.devicePixelRatio || 1);// 当前窗口的宽度
-                // console.log(wW);
+                console.log(wW);
                 var rem = wW * whdef;// 以默认比例值乘以当前窗口宽度,得到该宽度下的相应FONT-SIZE值
                 t.className = 's_pc';
                 var i = window.devicePixelRatio;
